2ccf76fd77a32c04cc657a3b9b3e66ad221b79fd
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2022-08-22 13:20:30 -0300

    adding_exercicios-2-3-4

--- exercicios_bloco4/exercicios_4.3-logica-algoritmos/exer-3.js
+++ exercicios_bloco4/exercicios_4.3-logica-algoritmos/exer-3.js
@@ -1,13 +1,16 @@
 let array = ['java', 'javascript', 'python', 'html', 'css'];
-let emptyVar = 0;
-let biggerIndex;  
-  
-for( let i = 0; i < array.length; i += 1){
-     if(array[i].length > emptyVar){
-     emptyVar = array[i].length;
+let biggerIndex= array [0];  
+let smallerIndex = array [0];  
+   for( let i = 0; i < array.length; i += 1){
+     if(array[i].length > biggerIndex.length){
      biggerIndex = array[i];
-     
-    }
-
-  }
+}
+   }
+   
+   for( let index = 0; index < array.length; index += 1){
+      if(array[index].length < smallerIndex.length){
+      smallerIndex = array[index];
+}   
+   }
     console.log(biggerIndex);
+    console.log(smallerIndex);
