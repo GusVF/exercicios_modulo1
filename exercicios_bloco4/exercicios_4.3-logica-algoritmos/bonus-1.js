7c6d4813f0baea47dd026672f5af0aac17b6459b
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2022-08-22 18:08:54 -0300

    exercises-1-2-3_+ao_vivo

---
+++ exercicios_bloco4/exercicios_4.3-logica-algoritmos/bonus-1.js
@@ -0,0 +1,12 @@
+let number = 8;
+let asteriscos = '*';
+let rows = '';
+
+  for (let i = 0; i < number; i += 1){
+      rows += asteriscos;
+  }
+    for (let i = 0; i < number; i += 1){
+      console.log(rows);
+  }
+
+   
