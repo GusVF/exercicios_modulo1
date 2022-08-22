8e47f27d7b715f26e36bc35e8e4ac16d22356444
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2022-08-22 11:40:17 -0300

    adding_exercicises

---
+++ exercicios_bloco4/exercicios_4.3-logica-algoritmos/exer-1.js
@@ -0,0 +1,7 @@
+
+let fatorial = 10;
+
+for (let i = 9; i > 0; i -= 1){
+    fatorial *= i;
+    console.log(fatorial)
+}
---
+++ exercicios_bloco4/exercicios_4.3-logica-algoritmos/exer-2.js
@@ -0,0 +1,8 @@
+let word = 'tryber';
+let backwards=[];
+
+for (let i = word.length -1; i >= 0; i -= 1){
+    backwards += word[i];
+    console.log(backwards);
+}
+
