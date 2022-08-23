fd4faffee0834af2f65735df104632d4c4844efb
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2022-08-23 19:41:10 -0300

    melhorando-triangulo

--- exercicios_bloco4/exercicios_4.4-objetos-funcoes/exer5-bloco4.js
+++ exercicios_bloco4/exercicios_4.4-objetos-funcoes/exer5-bloco4.js
@@ -1,5 +1,5 @@
 function triangle(angle1, angle2, angle3){
-    if (angle1 && angle2 && angle3 < 0){
+    if (angle1 < 0 || angle2 < 0 || angle3 < 0){
         return 'Erro: digite numeros positivos.';
     }else if (angle1 + angle2 + angle3 !== 180){
         return false;
