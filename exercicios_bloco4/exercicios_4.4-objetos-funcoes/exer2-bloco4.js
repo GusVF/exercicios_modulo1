216d834900b2eb9d7d364ea21a77bfdfe2a9a6fb
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2022-08-23 19:37:22 -0300

    exercicios_bloco4

---
+++ exercicios_bloco4/exercicios_4.4-objetos-funcoes/exer2-bloco4.js
@@ -0,0 +1,8 @@
+function biggerNumber(num1, num2) {
+    if(num1 > num2){
+        return num1 + ' é o maior numero!';
+   }
+        return num2 + ' é o maior numero!';
+}
+
+console.log(biggerNumber(120, 30));
