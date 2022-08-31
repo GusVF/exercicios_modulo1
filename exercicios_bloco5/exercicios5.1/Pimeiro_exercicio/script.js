89dfb26711ddf2697ee4fec465fb946101b6c50a
Gustavo Ferreira <110786890+GusVF@users.noreply.github.com>
2022-08-31 07:42:07 -0300

    Update script.js

--- exercicios_bloco5/exercicios5.1/Pimeiro_exercicio/script.js
+++ exercicios_bloco5/exercicios5.1/Pimeiro_exercicio/script.js
@@ -1,15 +1,25 @@
 
 
-document.getElementById("header-container").style.backgroundColor = 'green';
+const tituloH1 = document.getElementById('header-container');
+tituloH1.style.backgroundColor = 'rgb(76,164,46)';
 
-document.getElementsByClassName("emergency-tasks ")[0].style.backgroundColor = 'orange';
-document.getElementsByTagName('h3')[0].style.backgroundColor = 'purple';
-document.getElementsByTagName('h3')[1].style.backgroundColor = 'purple';
+const emergencyTask = document.getElementsByClassName('emergency-tasks')[0];
+emergencyTask.style.backgroundColor = 'rgb(255,178,102)';
 
-document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = 'yellow';
-document.getElementsByTagName('h3')[2].style.backgroundColor = 'black';
-document.getElementsByTagName('h3')[3].style.backgroundColor = 'black';
+const noEmergencyTask = document.getElementsByClassName('no-emergency-tasks')[0];
+noEmergencyTask.style.backgroundColor = 'rgb(255,204,153)';
 
+const h3UrgEImport = document.getElementsByTagName('h3')[0];
+h3UrgEImport.style.backgroundColor = 'rgb(204,0,204)';
 
-document.getElementById("footer-container").style.backgroundColor = 'black';
+const h3NonImport = document.getElementsByTagName('h3')[1];
+h3NonImport.style.backgroundColor = 'rgb(204,0,204)';
 
+const h3NonUImport = document.getElementsByTagName('h3')[2];
+h3NonUImport.style.backgroundColor = 'black';
+
+const h3NonUrgentImpor = document.getElementsByTagName('h3')[3];
+h3NonUrgentImpor.style.backgroundColor = 'black';
+
+const footer = document.querySelectorAll('footer')[0];
+footer.style.backgroundColor = 'black';
